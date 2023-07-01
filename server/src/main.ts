d94f7b55276ded1ca8038ba7e4e6b8b311d9cac6
shoolpani dubey <shool.pani.dubey@gmail.com>
2023-07-01 23:58:28 +0300

    server port config updated

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// Use port number from the PORT environment variable or 3000 if not specified
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
